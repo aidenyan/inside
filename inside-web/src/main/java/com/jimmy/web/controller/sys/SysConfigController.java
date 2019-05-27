0b41334fbedfb3d4887d4558fe991d524fa65af5
yanyifei <yanyifei@zcckj.com>
2019-05-27 16:04:59 +0800

    初始化

---
+++ inside-web/src/main/java/com/jimmy/web/controller/sys/SysConfigController.java
@@ -0,0 +1,9 @@
+package com.jimmy.web.controller.sys;
+
+import com.jimmy.web.controller.BaseController;
+
+/**
+ * Created by Administrator on 2019/5/27/027.
+ */
+public class SysConfigController  extends BaseController {
+}
